5bf145c1c0342414576518dc6ec178c53b47ad67
kushendra1 <kushendra.ramrup59@myhunter.cuny.edu>
2021-04-11 14:23:56 -0400

    Lat and LOng

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
-export const City = ({ City, State, EstimatedPopulation, TotalWages }) => {
+export const City = ({ City, State, Lat, Long, EstimatedPopulation, TotalWages }) => {
     return (
         <div>
             <ul>
                 <li>City: {City}</li>
                 <li>State: {State}</li>
+                <li>Location: {Lat}, {Long}</li>
                 <li>Estimated Population: {EstimatedPopulation}</li>
                 <li>Total Wages: ${TotalWages}</li>
             </ul>
