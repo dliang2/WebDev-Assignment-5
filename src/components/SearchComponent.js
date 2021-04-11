bcb71ec268ced34adb8b0ba56aed73e07dcc32a8
kushendra1 <kushendra.ramrup59@myhunter.cuny.edu>
2021-04-11 13:53:42 -0400

    maketable

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -17,14 +17,14 @@ class SearchAPI extends Components {
 
   handleSearchClick = async () => {
     let zipcodeName = this.state.zipcode;
-    let linkToAPI = 'http://ctp-zip-api.herokuapp.com/zip/:zipcode'+zipcodeName;
+    let linkToAPI = 'http://ctp-zip-api.herokuapp.com/zip/'+zipcodeName;
     try {
-      let res = await axios.get(linkToAPI);
-      this.setState({apiData: res.data, found: true});
+      let response = await axios.get(linkToAPI);
+      this.setState({apiData: response.data, found: true});
     } catch (error) {
       if (error.response) {
-        console.log(error.response.data);
-        console.log(error.response.status);
+        console.log(error.response.data); //not found
+        console.log(error.response.status); //404
         this.setState({found: false});
       }
     }
@@ -36,11 +36,26 @@ class SearchAPI extends Components {
     let table = [];
 
     if (!foundMatch) {
-
+      table.push(<tr key={-1}><td>No Results</td></tr>);
+      return table;
     } else {
-
+      let LocationText = currData.LocationText;
+      let cState = currData.State;
+      let cLat = currData.Lat;
+      let cLong = currData.Long;
+      let cEstimatedPopulation = currData.EstimatedPopulation;
+      let cTotalWages = currData.TotalWages;
+      table.push(
+        <tr key={currData.id}>
+          <td>{LocationText}</td>
+          <td>State: {cState}</td>
+          <td>Location: {cLat}, {cLong}</td>
+          <td>Population: {cEstimatedPopulation}</td>
+          <td>Wages: {cTotalWages}</td>
+        </tr>
+      );
+      return table;
     }
-    return table;
   }
 
   render() {
@@ -48,7 +63,7 @@ class SearchAPI extends Components {
       <div className="container">
         <div className = "search">
           <h3>Search ZipCode:</h3>
-          <input type="text" value={this.state.zipcode} onChange={this.handleInputChange} placeholder="Enter Zipcode" </input>
+          <input type="text" value={this.state.zipcode} onChange={this.handleInputChange} placeholder="Enter Zipcode"/>
           <button className="search-button" onClick={this.handleSearchClick}>Search</button>
         </div>
         <br/>
